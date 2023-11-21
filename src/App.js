2dcc08781e947abe3b974d41093ecafc7338cf91
Oladeji Anthony <oladejianthony98@gmail.com>
2023-11-21 13:18:40 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css'
+import Box1 from './components/Box1';
+import Box2 from './components/Box2';
+import Box3 from './components/Box3';
+import Box4 from './components/Box4';
+import Box5 from './components/Box5';
+import Box6 from './components/Box6';
+import Box7 from './components/Box7';
+import Sta from './components/Sta';
+
+function App() {
+  return (
+    <div className='app'>
+      
+    </div>
+  );
+}
+
+export default App;
+
